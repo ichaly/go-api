226378d2fbcd81ed3788be7518123ea76e92d8e7
shi.changliang <shi.changliang@zhaopin.com.cn>
2022-08-10 00:06:26 +0800

    feat:添加获取项目根目录的工具方法

--- core/app/internal/util/util.go
+++ core/app/internal/util/util.go
@@ -1,8 +1,22 @@
 package util
 
+import (
+	"path"
+	"path/filepath"
+	"runtime"
+)
+
 func String(value string, defaultValue string) string {
 	if len(value) > 0 {
 		return value
 	}
 	return defaultValue
+}
+
+func Root() string {
+	_, filename, _, ok := runtime.Caller(0)
+	if ok {
+		return filepath.Dir(path.Join(path.Dir(filename), "../../"))
+	}
+	return ""
 }
