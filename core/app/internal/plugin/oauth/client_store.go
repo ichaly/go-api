56dbd432d8f8b76c70bbe9f6be07342a7533eb59
shi.changliang <shi.changliang@zhaopin.com.cn>
2022-11-07 15:23:51 +0800

    feat:优化服务内查询

--- core/app/internal/plugin/oauth/client_store.go
+++ core/app/internal/plugin/oauth/client_store.go
@@ -5,40 +5,27 @@ import (
 	"github.com/dosco/graphjin/serv"
 	"github.com/go-oauth2/oauth2/v4"
 	"github.com/go-oauth2/oauth2/v4/models"
-	"github.com/ichaly/go-api/core/app/pkg"
 	"github.com/json-iterator/go"
-	"github.com/tidwall/gjson"
+	"github.com/mitchellh/mapstructure"
+	"gorm.io/gorm"
 )
 
 var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 type ClientStore struct {
-	Engine *serv.Service
+	Engine   *serv.Service
+	Database *gorm.DB
 }
 
-func NewOauthClientStore(s *serv.Service) oauth2.ClientStore {
-	return &ClientStore{Engine: s}
+func NewOauthClientStore(d *gorm.DB, s *serv.Service) oauth2.ClientStore {
+	return &ClientStore{Engine: s, Database: d}
 }
 
 // GetByID http://127.0.0.1:8080/oauth/token?grant_type=client_credentials&client_id=0&client_secret=eV4YeKI484E1nVoioE91Os6eOQip0TFs&scope=read
 func (my *ClientStore) GetByID(ctx context.Context, id string) (oauth2.ClientInfo, error) {
-	gql := `query getClientByID($id: ID) {
-	  clientsByID(id: $id) {
-		id
-		secret
-		domain
-	  }
-	}`
-	ql, err := my.Engine.GetGraphJin().GraphQL(ctx, gql, core.Variable{
-		"id": id,
-	}.Marshal(), nil)
-	if err != nil {
-		return nil, err
-	}
 	var c *models.Client
-	raw := gjson.GetBytes(ql.Data, "clientsByID").Raw
-	if err = json.UnmarshalFromString(raw, &c); err != nil {
-		return nil, err
-	}
-	return c, nil
+	m := map[string]interface{}{}
+	my.Database.Table("clients").Where("id = ?", id).Take(&m)
+	err := mapstructure.WeakDecode(m, &c)
+	return c, err
 }
